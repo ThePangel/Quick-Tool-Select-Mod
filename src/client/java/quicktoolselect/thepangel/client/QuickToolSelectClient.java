41611b7d2d0ac2becb8dbf4ae9dfc1109e9d2c33
ThePangel <74447121+ThePangel@users.noreply.github.com>
2026-05-21 19:55:40 +0200

    project created

---
+++ src/client/java/quicktoolselect/thepangel/client/QuickToolSelectClient.java
@@ -0,0 +1,10 @@
+package quicktoolselect.thepangel.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuickToolSelectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
